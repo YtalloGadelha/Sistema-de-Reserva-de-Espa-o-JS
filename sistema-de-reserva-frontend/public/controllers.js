a338430650fd3e29a8791e7000e464c299ffd754
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2017-01-03 00:04:01 -0300

    Criação e implementação do (controller,servicos).js referente a tabela Espacos

--- sistema-de-reserva-frontend/public/controllers.js
+++ sistema-de-reserva-frontend/public/controllers.js
@@ -1,8 +1,8 @@
 angular.module("hellosystem").controller("pessoacontroller", function(pessoaservice){
 
-  this.novo = {};
+  this.nova_pessoa = {};
 
-  this.listar = () => eventoservice.buscapessoas().then( (ret) => {
+  this.listar = () => pessoaservice.buscapessoas().then( (ret) => {
     this.pessoas = ret.data;
   });
 
@@ -10,13 +10,34 @@ angular.module("hellosystem").controller("pessoacontroller", function(pessoaserv
   this.listar();
 
   this.salvapessoa = () => {
-    eventoservice.salvapessoa(this.novo).then( (ret) => {
+    pessoaservice.salvapessoa(this.nova_pessoa).then( (ret) => {
       alert("Pessoa salva com id "+ret.data.id_pessoa);
       this.listar();
-      this.novo = {};
+      this.nova_pessoa = {};
     });
   };
 });
+
+angular.module("hellosystem").controller("espacocontroller", function(espacoservice){
+
+  this.novo_espaco = {};
+
+  this.listar = () => espacoservice.buscaespaco().then( (ret) => {
+    this.espacos = ret.data;
+  });
+
+  // carregar a lista imediatamente após carregar o controlador
+  this.listar();
+
+  this.salvapessoa = () => {
+    espacoservice.salvaespacos(this.novo_espaco).then( (ret) => {
+      alert("Espaço salvo com id "+ret.data.id_espaco);
+      this.listar();
+      this.novo_espaco = {};
+    });
+  };
+});
+
 
 // roteamento
 angular.module("hellosystem").config(($routeProvider) => {
@@ -24,6 +45,12 @@ angular.module("hellosystem").config(($routeProvider) => {
   $routeProvider.when("/pessoas", {
     controller:"pessoacontroller",
     templateUrl:"pessoas.html",
+    controllerAs:"ctl"
+  });
+
+   $routeProvider.when("/espacos", {
+    controller:"espacocontroller",
+    templateUrl:"espacos.html",
     controllerAs:"ctl"
   });
 
--- sistema-de-reserva-frontend/public/servicos.js
+++ sistema-de-reserva-frontend/public/servicos.js
@@ -5,3 +5,11 @@ angular.module("hellosystem").service("pessoaservice", function ($http){
   this.salvapessoa = (ev) => $http.post("pessoas",ev);
 
 });
+
+angular.module("hellosystem").service("espacoservice", function ($http){
+
+  this.buscaespacos = () => $http.get("espacos");
+
+  this.salvaespaco = (ev) => $http.post("espacos",ev);
+
+});
