7c6cc0252b356a557814dde53398f7054d16acb3
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2017-01-04 00:34:19 -0300

    Criação das rotas(GET e POST) referente a tabela pessoas

---
+++ sistema-de-reserva/frontend/controllers.js
@@ -0,0 +1,58 @@
+angular.module("hellosystem").controller("pessoacontroller", function(pessoaservice){
+
+  this.nova_pessoa = {};
+
+  this.listar = () => pessoaservice.buscapessoas().then( (ret) => {
+    this.pessoas = ret.data;
+  });
+
+  // carregar a lista imediatamente após carregar o controlador
+  this.listar();
+
+  this.salvapessoa = () => {
+    pessoaservice.salvapessoa(this.nova_pessoa).then( (ret) => {
+      alert("Pessoa salva com id "+ret.data.id_pessoa);
+      this.listar();
+      this.nova_pessoa = {};
+    });
+  };
+});
+
+angular.module("hellosystem").controller("espacocontroller", function(espacoservice){
+
+  this.novo_espaco = {};
+
+  this.listar = () => espacoservice.buscaespaco().then( (ret) => {
+    this.espacos = ret.data;
+  });
+
+  // carregar a lista imediatamente após carregar o controlador
+  this.listar();
+
+  this.salvapessoa = () => {
+    espacoservice.salvaespacos(this.novo_espaco).then( (ret) => {
+      alert("Espaço salvo com id "+ret.data.id_espaco);
+      this.listar();
+      this.novo_espaco = {};
+    });
+  };
+});
+
+// roteamento
+angular.module("hellosystem").config(($routeProvider) => {
+
+  $routeProvider.when("/pessoas", {
+    controller:"pessoacontroller",
+    templateUrl:"pessoas.html",
+    controllerAs:"ctl"
+  });
+
+   $routeProvider.when("/espacos", {
+    controller:"espacocontroller",
+    templateUrl:"espacos.html",
+    controllerAs:"ctl"
+  });
+
+  $routeProvider.otherwise("/pessoas");
+
+});
--- sistema-de-reserva/index.js
+++ sistema-de-reserva/index.js
@@ -2,10 +2,30 @@
 const knexfile = require('./knexfile')["development"];
 const knex = require("knex")(knexfile);
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("frontend"));
+
 app.get("/hello", (req,res) => res.send("Inhaeããããã :P"));
+
+app.get("/pessoas", (req,res) => {
+  knex("pessoas").select().then((ret) => res.send(ret));
+});
+
+app.post("/pessoa", (req,res) => {
+  var nova_pessoa = req.body;
+  
+  knex("pessoas").insert(nova_pessoa,"nome_pessoa").then((ret) => {
+    nova_pessoa.id_pessoa = ret[0];
+    res.send(nova_pessoa);
+  }).catch((err) => {
+    res.status(500).send(err);
+  });
+});
 
 app.listen(3000);
 console.log("App online!");
